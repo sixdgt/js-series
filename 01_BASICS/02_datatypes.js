a8f100e0788c1792d2a00ed11b9a91b43c685a83
sixdgt <c4crypt@gmail.com>
2023-12-30 12:14:39 +0545

    Datatype Conversion

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all JS Code as newer version i.e old codes
+
+// alert("hello") // we are using nodejs not browser
+// follow code readability
+
+// documentation of JS
+// 1. https://tc39.es/ecma262/
+// 2. mdn i.e mozilla js documentations
+
+
+// datatypes
+// Primitive Datatypes
+// 1. string
+let name = "Sandesh Tamang"
+
+// 2. number
+let code = 123
+
+// 3. boolean
+let isLoggedIn = false
+let isActive = true
+
+// 4. null - standalone value
+let set = null
+
+// 5. undefined - declared value but not assigned
+let state;
+
+// 6. symbol - to find uniqueness
+
+// Non Primitive Datatypes
+// 1. object
+// 2. typeof
+// 3. undefined
+// 4. specification
+console.log(typeof state)
+console.log(typeof name)
+
+// Note: null is object whereas undefined itself is a type
---
+++ 01_BASICS/03_conversionOperation.js
@@ -0,0 +1,58 @@
+let score = 33
+let price = "1200"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+console.log(typeof(price))
+
+// number conversion 
+let valueInNumber = Number(price)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+// js can convert from one datatype into another but we need to be careful while
+// printing or using the value
+// eg
+let markedPrice = "1233 Rs."
+let convertedMarkedPrice = Number(markedPrice) // markedPrice can be easily convert
+
+console.log( typeof markedPrice) // here markedPrice is a string
+console.log(typeof convertedMarkedPrice) // here price converted to number with value NaN
+
+// when we print the converted data we get NaN
+console.log(markedPrice)
+console.log(convertedMarkedPrice)
+
+// in case of null
+let discount = null
+let discountPrice = Number(discount) // converted to Number with value 0
+
+console.log(discount)
+console.log(discountPrice) 
+
+// in case of undefined
+let vat = undefined
+let vatAmount = Number(vat) // converted to Number with value NaN
+
+console.log(vat)
+console.log(vatAmount) 
+
+// in case of boolean
+let isRemoved = true
+let isActive = Number(isRemoved) // converted to Number with value 1 or 0
+
+console.log(isRemoved)
+console.log(isActive) 
+
+// conversion number to boolean
+let isLoggedIn = 1 // 1 => true and 0 => false
+let loginStatus = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+console.log(loginStatus)
+
+// string conversion
+let num = 1
+let val = String(num)
+
+console.log(val)
+console.log(typeof val)
